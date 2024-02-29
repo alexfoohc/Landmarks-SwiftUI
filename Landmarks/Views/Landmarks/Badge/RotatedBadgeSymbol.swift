ec740ea9acef509b5d5708d91c28cf3b865f0da4
Alejandro Hernandez <alex.hernandezc.21@gmail.com>
2024-02-28 22:35:33 -0600

    Add badge elements

---
+++ Landmarks/Views/Landmarks/Badge/RotatedBadgeSymbol.swift
@@ -0,0 +1,23 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Alejandro Hernandez on 28/02/24.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
